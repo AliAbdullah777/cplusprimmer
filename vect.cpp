29d305aab867abcdcd75ca5353a5245a973a6a02
AliAbdullah <111515590+AliAbdullah777@users.noreply.github.com>
2022-09-01 17:15:23 +0500

    Add files via upload

---
+++ vect.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main ()
+{
+    string name = "Ali";
+    string name2 = "Abdullah";
+    cout << name + name2;
+    
+    
+    
+    return 0;
+   
+ }
